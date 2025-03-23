23889b023a21fa098d7d52e2dd3e4e8825580f01
kishore kumar <kishornaveen2193@GMAIL.COM>
2025-03-23 17:50:26 +0530

    Update route.ts

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -1,5 +1,7 @@
 import { convertToCoreMessages, Message, streamText } from "ai";
+
 import { z } from "zod";
+
 import { geminiProModel } from "@/ai";
 import { auth } from "@/app/(auth)/auth";
 import { deleteChatById, getChatById, saveChat } from "@/db/queries";
