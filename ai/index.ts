1fa4b9a7853d561b7cc4df3fe1ec1fcf0aef8286
kishore kumar <kishornaveen2193@GMAIL.COM>
2025-03-27 18:53:12 +0530

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -4,7 +4,7 @@ import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { customMiddleware } from "./custom-middleware";
 
 export const geminiProModel = wrapLanguageModel({
-  model: google("gemini-2.0-flash"),
+  model: google("gemini-1.5-pro"),
   middleware: customMiddleware,
 });
 
